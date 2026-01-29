e3498939adf85a41629be4324a1315b048d9972b
Merle Hoyer <mhoyer06@gmail.com>
2026-01-29 22:16:12 +0000

    layout for board

--- courses/coding-in-C/mySolutions/chessboard.c
+++ courses/coding-in-C/mySolutions/chessboard.c
@@ -4,23 +4,39 @@ int main(){
 
     int row[10] = {8,7,6,5,4,3,2,1};
     char column[10]= {"ABCDEFGH"};
+    int il=0;
 
 
-    for (int i=0; i<10; i ++){
-       
+    for (int i=0; i<11; i ++){
+       if(i%2==0){
+        for(int k=0; k<8; k++){
+        printf("+---");
+        }
+        printf("+");
+       }else {
+        
         for (int j=0; j<8; j++){
-            if(i==0 && 0<j<9){
+           if(i==1 && 0<j<9){
                 printf(" %c ", column[(j-1)]);
-            }
-            if ((i+j)%2==0){
-                printf("|##|");
-
-            } else {
-                printf("   ");
+            } else{
+            printf("|");
+        
+                    
+                if ((il+j)%2==0){
+                    printf("###");
+                } 
+                else {
+                    printf("   ");
+                }
+                
             }
             //printf("%c",column[j]);
-            //printf("%d ",row[i]);
+            //printf("%d ",row[i]);*/
             
+        }
+            il++;
+            printf("|");
+
         }
         
             printf("\n");
