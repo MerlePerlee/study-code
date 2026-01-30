cf47be3d02f1a7dfc5ee5c440c3be360e917aa89
Merle Hoyer <mhoyer06@gmail.com>
2026-01-30 14:58:44 +0000

    finish boarder

--- courses/coding-in-C/mySolutions/chessboard.c
+++ courses/coding-in-C/mySolutions/chessboard.c
@@ -5,21 +5,26 @@ int main(){
     int row[10] = {8,7,6,5,4,3,2,1};
     char column[10]= {"ABCDEFGH"};
     int il=0;
+    int iboardsize= 9;
 
 
-    for (int i=0; i<11; i ++){
+    for (int i=0; i<(iboardsize+7); i ++){
        if(i%2==0){
-        for(int k=0; k<8; k++){
-        printf("+---");
-        }
-        printf("+");
-       }else {
         
-        for (int j=0; j<8; j++){
-           if(i==1 && 0<j<9){
+        for (int j=0; j<iboardsize; j++){
+           
+            if(i==0&& j==0){
+                printf(" ");
+            }
+            else if(i==0&& j!=0){
+                printf(" ");
                 printf(" %c ", column[(j-1)]);
-            } else{
-            printf("|");
+                
+            } else
+            {
+               
+             if(j!=0){
+                printf("|");
         
                     
                 if ((il+j)%2==0){
@@ -28,21 +33,45 @@ int main(){
                 else {
                     printf("   ");
                 }
-                
+             }else{
+                printf("%d",il);
+             }
             }
             //printf("%c",column[j]);
             //printf("%d ",row[i]);*/
             
         }
-            il++;
+            
+            if(i!=0){
             printf("|");
+            printf(" %d",il);
+            }
+            il++;
 
+        }else
+        {
+        
+        for(int k=0; k<(iboardsize); k++){
+            if(k!=0){
+            printf("+---");
+            } else{
+                printf(" ");
+            }
         }
+        printf("+");
+       }
         
             printf("\n");
+
+               
+    
     }
+    
 
+ for(int m=0; m < 8; m++){
+        printf(" ");
+        printf(" %c ", column[(m)]);
 
-
+ }
     return 0;
 }
